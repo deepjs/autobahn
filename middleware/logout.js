5c9ce45cd0064e102551c9733fe0c12d571ad797
Gilles Coomans <gilles.coomans@gmail.com>
2013-12-04 15:20:56 +0100

    add logout middleware

---
+++ middleware/logout.js
@@ -0,0 +1,12 @@
+
+var urlparse = require('url').parse;
+var crypto = require('crypto');
+exports.middleware = function(userStore, loginField, successHandler){
+	return function (req, response, next)
+	{
+		if(req.session)
+			req.session.destroy();
+		response.writeHead(200, {'Content-Type': 'application/json'});
+		response.end(JSON.stringify(true));
+	};
+};
