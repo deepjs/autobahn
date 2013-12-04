7e419eea9d416420024070dd60c73a56b1e61844
Gilles Coomans <gilles.coomans@gmail.com>
2013-12-04 15:20:08 +0100

    guard with no successHandler

--- middleware/login.js
+++ middleware/login.js
@@ -5,9 +5,10 @@ exports.middleware = function(userStore, loginField, successHandler){
 	loginField = loginField || "email";
 	return function (req, response, next)
 	{
+		//console.log("LOGIN : ", req.body);
 		if(!req.body || !req.body.password || !req.body[loginField])
 		{
-			response.writeHead(error.status || 400, {'Content-Type': 'text/html'});
+			response.writeHead(400, {'Content-Type': 'text/html'});
 			response.end("error : misformed body provided for login");
 			return;
 		}
@@ -22,10 +23,17 @@ exports.middleware = function(userStore, loginField, successHandler){
 			success = success.shift();
 			delete success.password;
 			req.session.user = success;
-			deep.when(successHandler(success)).done(function(success){
+			if(successHandler)
+				deep.when(successHandler(success))
+				.done(function(success){
+					response.writeHead(200, {'Content-Type': 'application/json'});
+					response.end(JSON.stringify(success));
+				});
+			else
+			{
 				response.writeHead(200, {'Content-Type': 'application/json'});
 				response.end(JSON.stringify(success));
-			});
+			}
 		})
 		.fail(function(error){
 			response.writeHead(error.status || 400, {'Content-Type': 'text/html'});
