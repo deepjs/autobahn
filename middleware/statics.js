e1dc88292427e5d3d62e4be23d19878cb46283a9
Gilles Coomans <gilles.coomans@gmail.com>
2013-12-12 09:20:00 +0100

    Full rewrite of statics middleware. Manage cleanly OCM. debug context with EventEmitter

--- middleware/statics.js
+++ middleware/statics.js
@@ -4,12 +4,189 @@
  */
 var deep = require("deepjs");
 var express = require('express');
-exports.map = function(map, app){
+var router = require("deep-routes/route");
+var urlparse = require('url').parse;
+
+
+function produceMapper(map)
+{
 	var keys = Object.keys(map);
+	var routes = [];
 	keys.forEach(function(i){
 		var entries = map[i];
+		var handler = {
+			path:i,
+			middlewares:[]
+		};
+		routes.push(handler);
 		entries.forEach(function(entry){
-			app.use(i, express.static(entry.path, entry.options));
+			handler.middlewares.push(CreateManager(entry.path, i, entry.options));
 		});
 	});
+	return function(req, res, next)
+	{
+		var firstURL = req.url;
+		var pathname = urlparse(req.url).pathname;
+		var middlewares = [];
+		routes.forEach(function (entry) {
+			//console.log("statics ananlyse : ", pathname, entry.path);
+			if(pathname.match(new RegExp("^("+entry.path+")")))
+				middlewares = middlewares.concat(entry.middlewares);
+		});
+		var context = deep.context;
+		var hackedNext = function(){
+			deep.context = context;
+			if(middlewares.length === 0)
+				return next();
+			var follow = middlewares.shift();
+			follow(req, res, hackedNext);
+		};
+		hackedNext();
+	};
+}
+
+exports.middleware = function(map){
+	var closure = {
+		cache:null
+	};
+	var getMap = function (){
+		if(map._deep_ocm_)
+		{
+			closure.cache = closure.cache || {};
+			if(deep.context.modes && deep.context.modes.roles)
+			{
+				var joined = deep.context.modes.roles;
+				if(joined.join)
+					joined.join(".");
+				if(closure.cache[joined])
+					return closure.cache[joined];
+				closure.cache[joined] = produceMapper(map(),{});
+				return closure.cache[joined];
+			}
+			else
+				return deep.errors.OCM("Statics map is OCM but no roles found. aborting.");
+		}
+		else if(closure.cache)
+			return closure.cache;
+		closure.cache = produceMapper(map, {});
+		return closure.cache;
+	};
+	return function (req, res, next)
+	{
+		var map = getMap();
+		if(map instanceof Error)
+		{
+			if(map.status == 404)
+				return next();
+			deep.utils.dumpError(map);
+			res.writeHead(map.status || 500, {'Content-Type': 'application/json'});
+			res.end(JSON.stringify(map));
+		}
+		else
+			map(req, res, next);
+	};
 };
+
+/*!
+ * Connect - static
+ * Copyright(c) 2010 Sencha Inc.
+ * Copyright(c) 2011 TJ Holowaychuk
+ * MIT Licensed
+ */
+
+/**
+ * Module dependencies.
+ */
+
+var send = require('send')
+  , utils = require('connect/lib/utils')
+  , parse = utils.parseUrl
+  , url = require('url');
+
+/**
+ * Static:
+ *
+ *   Static file server with the given `root` path.
+ *
+ * Examples:
+ *
+ *     var oneDay = 86400000;
+ *
+ *     connect()
+ *       .use(connect.static(__dirname + '/public'))
+ *
+ *     connect()
+ *       .use(connect.static(__dirname + '/public', { maxAge: oneDay }))
+ *
+ * Options:
+ *
+ *    - `maxAge`     Browser cache maxAge in milliseconds. defaults to 0
+ *    - `hidden`     Allow transfer of hidden files. defaults to false
+ *    - `redirect`   Redirect to trailing "/" when the pathname is a dir. defaults to true
+ *    - `index`      Default file name, defaults to 'index.html'
+ *
+ * @param {String} root
+ * @param {Object} options
+ * @return {Function}
+ * @api public
+ */
+
+function CreateManager(root, fakePath, options){
+  options = options || {};
+
+  // root required
+  if (!root) throw new Error('static() root path required');
+
+  // default redirect
+  var redirect = false !== options.redirect;
+
+  return function staticMiddleware(req, res, next) {
+    if ('GET' != req.method && 'HEAD' != req.method) return next();
+    var path = parse(req).pathname;
+    var pause = utils.pause(req);
+
+    if(path.length > fakePath.length)
+		path = path.substring(fakePath.length);
+
+    function resume() {
+      next();
+      pause.resume();
+    }
+
+    function directory() {
+      if (!redirect) return resume();
+      var originalUrl = url.parse(req.originalUrl);
+      var target;
+      originalUrl.pathname += '/';
+      target = url.format(originalUrl);
+      res.statusCode = 303;
+      res.setHeader('Location', target);
+      res.end('Redirecting to ' + utils.escape(target));
+    }
+
+    function error(err) {
+      if (404 == err.status) return resume();
+      next(err);
+    }
+
+    send(req, path)
+      .maxage(options.maxAge || 0)
+      .root(root)
+      .index(options.index || 'index.html')
+      .hidden(options.hidden)
+      .on('error', error)
+      .on('directory', directory)
+      .pipe(res);
+  };
+}
+
+/**
+ * Expose mime module.
+ *
+ * If you wish to extend the mime table use this
+ * reference to the "mime" module in the npm registry.
+ */
+
+exports.mime = send.mime;
+
+
