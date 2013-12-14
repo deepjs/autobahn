eb9fb2a2c7b00b54b8d072a2dabbd07c59ccab3a
Gilles Coomans <gilles.coomans@gmail.com>
2013-12-14 11:51:10 +0100

    clean roles middleware

--- middleware/roles.js
+++ middleware/roles.js
@@ -8,29 +8,17 @@ exports.middleware = function(getRoles){
 
 	return function (req, response, next)
 	{
-
-		if(req.session)
-		{
-			deep.when(getRoles(req.session))
-			.done(function (roles) {
-				//deep.setModes({ roles:roles });
-				deep.modes('roles',roles)
-				.done(function(){
-					//console.log("roles get : ", roles, deep.context.modes);
-					next();
-				});
-			})
-			.fail(function(e){
-				console.log("autobahn session middleware error : ", e.toString());
-				response.writeHead(e.status || 400, {'Content-Type': 'text/html'});
-				response.end("error : "+JSON.stringify(e));
-			});
-		}
-		else
-			deep.modes('roles',"public")
+		deep.when(getRoles(req.session))
+		.done(function (roles) {
+			deep.modes('roles',roles)
 			.done(function(){
-				//deep.setModes({ roles:"public" });
 				next();
 			});
+		})
+		.fail(function(e){
+			console.log("autobahn roles middleware error : ", e.toString());
+			response.writeHead(e.status || 400, {'Content-Type': 'text/html'});
+			response.end("error : "+JSON.stringify(e));
+		});
 	};
 };
