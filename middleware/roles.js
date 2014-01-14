ee8d94cc61f1df43291c80c32b4dda58d900740d
Gilles Coomans <gilles.coomans@gmail.com>
2014-01-14 19:34:16 +0100

    add fail management in roles mw

--- middleware/roles.js
+++ middleware/roles.js
@@ -10,13 +10,19 @@ exports.middleware = function(getRoles){
 	{
 		deep.when(getRoles(req.session))
 		.done(function (roles) {
+			// console.log("middleware roles : roles getted : ", roles)
 			deep.modes('roles',roles)
 			.done(function(){
+				//console.log("roles middleware : execute next", this._context.modes)
 				next();
-			});
+			})
+			.fail(function (e) {
+				console.log("set modes fail : ", e);
+				deep.utils.dumpError(e);
+			})
 		})
 		.fail(function(e){
-			console.log("autobahn roles middleware error : ", e.toString());
+			// console.log("autobahn roles middleware error : ", e.toString());
 			response.writeHead(e.status || 400, {'Content-Type': 'text/html'});
 			response.end("error : "+JSON.stringify(e));
 		});
