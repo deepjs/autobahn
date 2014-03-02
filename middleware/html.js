b9c0e5381f59c1d98469608255f5de1812926bbb
Gilles Coomans <gilles.coomans@gmail.com>
2014-03-02 21:33:55 +0100

    rewrote entirely html middleware. use deep.RouteNode. use cheerio. produce pages.

--- middleware/html.js
+++ middleware/html.js
@@ -3,43 +3,38 @@
  *
  */
 var deep = require("deepjs");
-var router = require("deep-routes/lib/flat");
+require("deep-routes");
+require("deep-jquery");
 var urlparse = require('url').parse;
+var cheerio = require('cheerio');
 exports.simpleMap = function(map){
+
+	var closure = {};
+
 	var usableMap = [];
-	for(var i in map)
+	var d = deep.createRouteMap(map)
+	.done(function(map){
+		closure.map = map;
+	})
+	.logError();
+
+	deep.jquery.addDomProtocols();
+	return function (request, response, next)
 	{
-		var r = router.createRouter(i, false);
-		usableMap.push({
-			router:r,
-			object:map[i]
-		});
-		if(map[i]._deep_ocm_)
-			map[i].flatten().logError();
-	}
-	return function (req, res, next)
-	{
-		// console.log("html mappers : ", req.url)
-		var pathname = urlparse(req.url).pathname;
-		var items = {};
-		var handled = usableMap.some(function (entry) {
-			items.params = entry.router.match(pathname);
-			if(items.params)
-			{
-				items.object = deep.utils.copy(entry.object);
-				return true;
-			}
-			return false;
-		});
-		
-		if(handled)
-		{
-			//console.log("html handled : ", items, handled);
-			var obj = items.object;
-			if(obj._deep_ocm_)
-				obj = obj();
-			deep(obj)
-			.deepLoad(items.params, false)
+		//console.log("html mappers : ", request.url , " - ", request.headers);
+		//if(!request.is("text/html"))
+		//	return next();
+		var $ = deep.context.$ = cheerio.load('<!doctype html><html><head></head><body></body></html>');
+		deep.jquery.init(deep.context.$);
+
+		deep.when(closure.map.route(request.url))
+		.always(function(){
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.end($.html());
+		})
+		//console.log("res : ", res)
+
+		/*
 			.done(function(success){
 				//console.log("success map loaded : ", pathname, success);
 				if(success.context.content && success.context.content.join)
@@ -55,6 +50,77 @@ exports.simpleMap = function(map){
 			});
 		}
 		else
-			next();
+			next();*/
 	};
 };
+
+/*
+    deep.generalModes("roles","public");
+
+    var exampleMap = {
+        topbar:deep.View({
+            route:"!/[login,register]",
+            how:"swig::/test.swig",
+            where:"dom.htmlOf::#topbar"
+        }),
+        footer:deep.ocm({
+            user:deep.View({
+                route:"!/register",
+                how:"<span>ocm powaaaaaaa user</span>",
+                where:deep.jquery.htmlOf("#footer")
+            }),
+            "public":{
+                backgrounds:["this::../user"],
+                how:"<span>ocm powaaaaaaa public</span>"
+            }
+        }, { group:"roles" }),
+        campaign:deep.View({
+            route:"/campaign/s:id",
+            how:"<div>hello campaign { id }<div id='info'></div><div id='update'></div></div>",
+            where:"dom.htmlOf::#content",
+            subs:{
+                info:deep.View({
+                    route:"?./info/s:id",
+                    how:"swig::/info.swig",
+                    where:"dom.htmlOf::#info"
+                }),
+                update:deep.View({
+                    route:"./update/s:id",
+                    how:"<div>update hello { id }<div id='profile'></div></div>",
+                    where:"dom.htmlOf::#update",
+                    subs:{
+                        profile:deep.View({
+                            how:"<div>profile from update { parent.id }</div>",
+                            where:"dom.htmlOf::#profile"
+                        })
+                    }
+                })
+            }
+        }),
+        register:deep.View({
+            route:"/register",
+            how:"<div>register</div>",
+            where:"dom.htmlOf::#content"
+        }),
+        campaigns:deep.View({
+            route:"/campaigns/?q:query/?(i:start/i:end)",
+            how:"<div>campaigns { query | 'no query' } - { start | '0' }:{ end | '10'}<div id='info'></div></div>",
+            where:"dom.htmlOf::#content",
+            subs:{
+              info:deep.View({
+                route:"?./info/s:id",
+                how:"swig::/info.swig",
+                where:"dom.htmlOf::#info"
+              })
+            }
+        })
+    };
+
+
+*/
+
+
+
+
+
+
