3315824096e71faea7ae20045d945a4a12da6f8f
Gilles Coomans <gilles.coomans@gmail.com>
2014-09-17 21:16:48 +0200

    update service map example

--- example/maps/services.js
+++ example/maps/services.js
@@ -3,14 +3,29 @@
  */
 var deep = require("deepjs/deep");
 require("deep-mongo");
+require("autobahnjs/lib/utils"); /// load deep.transformers.Hash
+
+var schema = {
+	properties:{
+		email:{ type:"string", format:"email", required:true },
+		password:{ type:"string", minLength:6, required:true, transformer:deep.transformers.Hash }
+	}
+};
+
 module.exports = {
 	"/user": deep.ocm({
-		admin: deep.Mongo(null, "mongodb://127.0.0.1:27017/testdb", "user"),
+		admin: deep.Mongo(null, "mongodb://127.0.0.1:27017/testdb", "user", schema),
 		user: {
-			backgrounds: ["this::../admin", deep.Disallow("del", "flush")]
+			_backgrounds: ["this::../admin", deep.AllowOnly("get")],
+			schema:{
+				ownerRestriction:"id", // user could only see his own user object.
+				properties:{
+					email:{ readOnly:true },
+					password:{ "private":true, readOnly:true }
+				}
+			}
 		},
 		"public": {
-			backgrounds: ["this::../user", deep.AllowOnly("get")]
 		}
 	}, {
 		protocol: "user",
